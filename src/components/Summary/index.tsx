905a03c7f7da83e0031b01a7957b7f8173888bad
Henrique <henriqueadlima@hotmail.com>
2022-05-31 23:17:58 -0300

    Erro title

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
+export function Summary(){
+  const { transactions } = useContext(TransactionsContext);
+
+  console.log(transactions);
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          R$1000
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>
+          - R$5000
+        </strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>
+          R$500
+        </strong>
+      </div>
+    </Container>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models:{
+    transactions: Model
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Dev',
+          amount: '2000',
+          createdAt: new Date('2021-01-01 09:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Dev',
+          amount: '2000',
+          createdAt: new Date('2021-01-01 09:00:00')
+        }
+
+         
+      ]
+    })
+  },
+
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transactions');
+    })
+  
+    this.post('/transactions', (schema, request)=> {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +58,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
